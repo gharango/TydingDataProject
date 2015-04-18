7ead2ba3a87e6dbc792e15494a0f38c6b15d8c6d
gharango <gustavo.arango@oracle.com>
2015-04-18 16:37:44 +0200

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,32 @@
+#1
+xtest=read.csv("test/X_test.txt", header=FALSE, sep="")
+xtrain=read.csv("train/X_train.txt", header=FALSE, sep="")
+
+ytest=read.csv("test/y_test.txt", col.names=c("activity"), header=FALSE, sep="")
+ytrain=read.csv("train/y_train.txt",col.names=c("activity"), header=FALSE, sep="")
+
+subjecttrain=read.csv("train/subject_train.txt", col.names=c("subject"), header=FALSE, sep="")
+subjecttest=read.csv("test/subject_test.txt", col.names=c("subject"), header=FALSE, sep="")
+
+xall = rbind(xtrain, xtest) 
+yall = rbind(ytrain, ytest)
+subjectall = rbind(subjecttrain, subjecttest)
+#2
+features=read.csv("features.txt", header=FALSE, sep="")
+names(features)=c("id","feature")
+
+vectExtract <-grep("mean\\(\\)|std\\(\\)",features$feature)
+
+library("dplyr")
+xallExtract = select(xall, one_of(paste0("V",as.character(vectExtract))))
+#3
+activity_labels=read.csv("activity_labels.txt", col.names=c("id","label"),header=FALSE, sep="")
+yall$activity = sapply(yall$activity, function(x){as.character(activity_labels$label[x])})
+
+xallExtract$activity = yall$activity
+xallExtract$subject = subjectall$subject
+#4
+names(xallExtract) = c(gsub("[-\\(\\)\\,]","",grep("mean\\(\\)|std\\(\\)",features$feature,value=TRUE)),"activity","subject")
+#5
+tidySet <- summarise_each(group_by(xallExtract, activity,subject),funs(mean), vars=-c(activity,subject))
+write.table(tidySet, file="tidyset.txt", row.name=FALSE)
